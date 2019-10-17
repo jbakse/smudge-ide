a8a1984513aadb9f56fe013413196ff4df18e52f
Justin Bakse <justin@tsfim.com>
2019-10-17 14:12:47 -0400

    start add sketches

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,6 +2,7 @@
     <div v-if="user" class="profile">
         <h1 v-if="user">About {{user.displayName}}</h1>
         <img class="user-photo" v-bind:src="user.photoURL" />
+        <button v-on:click="createSketch">Create Sketch</button>
     </div>
 </template>
 
@@ -10,6 +11,12 @@
 import Vue from 'vue';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
+
+const db = firebase.firestore();
+const users = db.collection('users');
+
+console.log('db', db);
 
 export default Vue.extend({
     name: 'Profile',
@@ -17,14 +24,40 @@ export default Vue.extend({
 
     data: () => ({
         user: null as (firebase.User | null),
+        users: [],
     }),
-    methods: {},
+
+    firestore: {
+        users: users,
+    },
+
+    methods: {
+        createSketch() {
+            users.add({
+                title: 'untitled',
+                content: '',
+            });
+        },
+    },
     mounted() {
         firebase.auth().onAuthStateChanged((user) => {
             this.user = user;
         });
     },
 });
+
+/*
+
+users[
+    {
+        sketches: [
+            title: "untitled"
+            content: ""
+        ]
+    }
+]
+
+*/
 </script>
 
 
@@ -34,3 +67,5 @@ export default Vue.extend({
     border-width: 20px 20px 60px 20px;
 }
 </style>
+
+
